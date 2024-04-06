80ecd770c7b2d31a5f6b11700d6c5268176d774a
Abdulrehman <you@example.com>
2024-04-06 15:11:45 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+var num1 = 8;
+var num2 = 9;
+//This will add the num1 and num2 (This is comment in Typescript
+console.log(num1 + num2);
---
+++ main.ts
@@ -0,0 +1,6 @@
+const num1: number = 8
+const num2: number = 9
+
+//This will add the num1 and num2 (This is comment in Typescript
+
+console.log(num1 + num2);    
